e377945da19c8d9dbaedc9da47dda559f5e65e83
jparep <parepson@gmail.com>
2024-12-19 16:40:34 -0700

    Method to check for Palindrone determine implemented

--- code.py
+++ code.py
@@ -2,5 +2,9 @@
 
 """Reverse a String Write a Python function to reverse a string."""
 def reverse_string(s:str) -> str:
-    # I just updated this part
     return s[::-1]
+
+""" Check for Palindrome Determine if a string is a palindrome""".
+def is_palindrone(s: str) -> str:
+    return s == s[::-1]
+
